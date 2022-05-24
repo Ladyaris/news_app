94c525201ab77210ec01a13a2a772c597530aaac
Ladyaris Khalishah <ladyaris.khalisah@gmail.com>
2022-05-24 14:31:08 +0700

    ganti nama data

--- purei/lib/models/about.api.dart
+++ purei/lib/models/about.api.dart
@@ -16,7 +16,7 @@ class AboutApi {
     Map data = jsonDecode(response.body);
     List _temp = [];
 
-    for (var i in data['feed']) {
+    for (var i in data['games']) {
       _temp.add(i['content']['details']);
     }
 
